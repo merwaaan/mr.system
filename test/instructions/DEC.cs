43f5b8f41919f1d6b67b15d33da004a5299ffb9e
Merwan Achibet <achibet.merwan@gmail.com>
2015-11-23 13:45:24 +0100

    Finish unit tests for INC and DEC

--- test/instructions/DEC.cs
+++ test/instructions/DEC.cs
@@ -8,14 +8,7 @@ namespace test.instructions {
 
     public DEC8() {
       opcodes = new byte[] {
-        0x05,
-        0x0D,
-        0x15,
-        0x1D,
-        0x25,
-        0x2D,
-        0x35,
-        0x3D
+        0x05, 0x0D, 0x15, 0x1D, 0x25, 0x2D, 0x35, 0x3D
       };
     }
 
@@ -26,57 +19,77 @@ namespace test.instructions {
     [InlineData(0x80, 0x7F)]
     [InlineData(0xFF, 0xFE)]
     public void ShouldDecrement(byte input, byte output) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(output, operands[0].Target));
+      AllOpcodes(
+        operands => operands[0].Target = input,
+        operands => Assert.Equal(output, operands[0].Target));
     }
 
     [Theory]
-    [InlineData(0, true)]
-    [InlineData(0x7e, false)]
-    [InlineData(0x7f, false)]
-    [InlineData(0x80, false)]
-    [InlineData(0x81, true)]
-    [InlineData(0xFE, true)]
-    [InlineData(0xFF, true)]
-    public void ShouldHandleSignFlag(byte input, bool sign) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(sign, cpu.Sign));
+    [InlineData(0, false, true)]
+    [InlineData(0, true, true)]
+    [InlineData(0x7e, true, false)]
+    [InlineData(0x7f, false, false)]
+    [InlineData(0x80, true, false)]
+    [InlineData(0x81, true, true)]
+    [InlineData(0xFE, false, true)]
+    [InlineData(0xFF, true, true)]
+    public void ShouldHandleSignFlag(byte input, bool signInput, bool signOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Sign = signInput;
+        },
+        operands => Assert.Equal(signOutput, cpu.Sign));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(1, true)]
-    [InlineData(0x98, false)]
-    [InlineData(0xFE, false)]
-    [InlineData(0xFF, false)]
-    public void ShouldHandleZeroFlag(byte input, bool zero) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(zero, cpu.Zero));
+    [InlineData(0, true, false)]
+    [InlineData(1, false, true)]
+    [InlineData(1, true, true)]
+    [InlineData(0x98, true, false)]
+    [InlineData(0xFE, false, false)]
+    [InlineData(0xFF, true, false)]
+    public void ShouldHandleZeroFlag(byte input, bool zeroInput, bool zeroOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Zero = zeroInput;
+        },
+        operands => Assert.Equal(zeroOutput, cpu.Zero));
     }
 
     [Theory]
-    [InlineData(0, true)]
-    [InlineData(0x2E, false)]
-    [InlineData(0x2F, false)]
-    [InlineData(0x30, true)]
-    [InlineData(0x80, true)]
-    [InlineData(0xFE, false)]
-    [InlineData(0xFF, false)]
-    public void ShouldHandleHalfCarryFlag(byte input, bool halfcarry) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(halfcarry, cpu.HalfCarry));
+    [InlineData(0, false, true)]
+    [InlineData(0x2E, true, false)]
+    [InlineData(0x2F, true, false)]
+    [InlineData(0x30, false, true)]
+    [InlineData(0x80, true, true)]
+    [InlineData(0xFE, false, false)]
+    [InlineData(0xFF, true, false)]
+    public void ShouldHandleHalfCarryFlag(byte input, bool hcarryInput, bool hcarryOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.HalfCarry = hcarryInput;
+        },
+        operands => Assert.Equal(hcarryOutput, cpu.HalfCarry));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(0x1C, false)]
-    [InlineData(0x7E, false)]
-    [InlineData(0x7F, false)]
-    [InlineData(0x80, true)]
-    [InlineData(0xFF, false)]
-    public void ShouldHandleOverflowFlag(byte input, bool overflow) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(overflow, cpu.Overflow));
+    [InlineData(0, true, false)]
+    [InlineData(0x1C, false, false)]
+    [InlineData(0x7E, false, false)]
+    [InlineData(0x7F, true, false)]
+    [InlineData(0x80, false, true)]
+    [InlineData(0x80, true, true)]
+    [InlineData(0xFF, false, false)]
+    public void ShouldHandleOverflowFlag(byte input, bool overflowInput, bool overflowOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Overflow = overflowInput;
+        },
+        operands => Assert.Equal(overflowOutput, cpu.Overflow));
     }
 
   }
@@ -85,10 +98,7 @@ namespace test.instructions {
 
     public DEC16() {
       opcodes = new byte[] {
-        0x0B,
-        0x1B,
-        0x2B,
-        0x3B
+        0x0B, 0x1B, 0x2B, 0x3B
       };
     }
 
@@ -100,10 +110,29 @@ namespace test.instructions {
     [InlineData(0xA456, 0xA455)]
     [InlineData(0xFFFF, 0xFFFE)]
     public void ShouldDecrement(ushort input, ushort output) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(output, operands[0].Target));
+      AllOpcodes(
+        operands => operands[0].Target = input,
+        operands => Assert.Equal(output, operands[0].Target));
     }
-    
+
+    [Theory]
+    [InlineData(0, "00000000")]
+    [InlineData(0x09, "11111111")]
+    [InlineData(0x7F, "11110000")]
+    [InlineData(0xFF, "10100110")]
+    [InlineData(0xA456, "00111110")]
+    [InlineData(0xFFFF, "01001001")]
+    public void ShouldNotAffectFlags(ushort input, string flags) {
+      byte f = Convert.ToByte(flags, 2);
+
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.registers.f = f;
+        },
+        operands => Assert.Equal(f, cpu.registers.f));
+    }
+
   }
 
 }
--- test/instructions/INC.cs
+++ test/instructions/INC.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Xunit;
-using mr.system;
 
 namespace test.instructions {
 
@@ -8,14 +7,7 @@ namespace test.instructions {
 
     public INC8() {
       opcodes = new byte[] {
-        0x04,
-        0x0C,
-        0x14,
-        0x1C,
-        0x24,
-        0x2C,
-        0x34,
-        0x3C
+        0x04, 0x0C, 0x14, 0x1C, 0x24, 0x2C, 0x34, 0x3C
       };
     }
 
@@ -25,54 +17,79 @@ namespace test.instructions {
     [InlineData(0x7F, 0x80)]
     [InlineData(0xFF, 0)]
     public void ShouldIncrement(byte input, byte output) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(output, operands[0].Target));
+      AllOpcodes(
+        operands => operands[0].Target = input,
+        operands => Assert.Equal(output, operands[0].Target));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(0x7e, false)]
-    [InlineData(0x7f, true)]
-    [InlineData(0x80, true)]
-    [InlineData(0xFE, true)]
-    [InlineData(0xFF, false)]
-    public void ShouldHandleSignFlag(byte input, bool sign) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(sign, cpu.Sign));
+    [InlineData(0, false, false)]
+    [InlineData(0, true, false)]
+    [InlineData(0x7e, true, false)]
+    [InlineData(0x7f, true, true)]
+    [InlineData(0x80, false, true)]
+    [InlineData(0xFE, false, true)]
+    [InlineData(0xFF, false, false)]
+    public void ShouldHandleSignFlag(byte input, bool signInput, bool signOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Sign = signInput;
+        },
+        operands => Assert.Equal(signOutput, cpu.Sign));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(0x98, false)]
-    [InlineData(0xFE, false)]
-    [InlineData(0xFF, true)]
-    public void ShouldHandleZeroFlag(byte input, bool zero) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(zero, cpu.Zero));
+    [InlineData(0, true, false)]
+    [InlineData(0, false, false)]
+    [InlineData(1, true, false)]
+    [InlineData(0x85, false, false)]
+    [InlineData(0xFE, true, false)]
+    [InlineData(0xFF, false, true)]
+    [InlineData(0xFF, true, true)]
+    public void ShouldHandleZeroFlag(byte input, bool zeroInput, bool zeroOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Zero = zeroInput;
+        },
+        operands => Assert.Equal(zeroOutput, cpu.Zero));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(0x2E, false)]
-    [InlineData(0x2F, true)]
-    [InlineData(0x80, false)]
-    [InlineData(0xFE, false)]
-    [InlineData(0xFF, true)]
-    public void ShouldHandleHalfCarryFlag(byte input, bool halfcarry) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(halfcarry, cpu.HalfCarry));
+    [InlineData(0, true, false)]
+    [InlineData(0x2E, false, false)]
+    [InlineData(0x2E, true, false)]
+    [InlineData(0x2F, false, true)]
+    [InlineData(0x8C, true, false)]
+    [InlineData(0xFE, false, false)]
+    [InlineData(0xFF, false, true)]
+    [InlineData(0xFF, true, true)]
+    public void ShouldHandleHalfCarryFlag(byte input, bool hcarryInput, bool hcarryOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.HalfCarry = hcarryInput;
+        },
+        operands => Assert.Equal(hcarryOutput, cpu.HalfCarry));
     }
 
     [Theory]
-    [InlineData(0, false)]
-    [InlineData(0x1C, false)]
-    [InlineData(0x7E, false)]
-    [InlineData(0x7F, true)]
-    [InlineData(0x80, false)]
-    [InlineData(0xFF, false)]
-    public void ShouldHandleOverflowFlag(byte input, bool overflow) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(overflow, cpu.Overflow));
+    [InlineData(0, false, false)]
+    [InlineData(0x1C, true, false)]
+    [InlineData(0x7E, false, false)]
+    [InlineData(0x7E, true, false)]
+    [InlineData(0x7F, false, true)]
+    [InlineData(0x7F, true, true)]
+    [InlineData(0x80, true, false)]
+    [InlineData(0xFF, false, false)]
+    public void ShouldHandleOverflowFlag(byte input, bool overflowInput, bool overflowOutput) {
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.Overflow = overflowInput;
+        },
+        operands => Assert.Equal(overflowOutput, cpu.Overflow));
     }
 
   }
@@ -81,10 +98,7 @@ namespace test.instructions {
 
     public INC16() {
       opcodes = new byte[] {
-        0x03,
-        0x13,
-        0x23,
-        0x33
+        0x03, 0x13, 0x23, 0x33
       };
     }
 
@@ -96,8 +110,26 @@ namespace test.instructions {
     [InlineData(0xA456, 0xA457)]
     [InlineData(0xFFFF, 0)]
     public void ShouldIncrement(ushort input, ushort output) {
-      AllOpcodes(operands => operands[0].Target = input,
-                 operands => Assert.Equal(output, operands[0].Target));
+      AllOpcodes(
+        operands => operands[0].Target = input,
+        operands => Assert.Equal(output, operands[0].Target));
+    }
+
+    [Theory]
+    [InlineData(0, "10010000")]
+    [InlineData(1, "11111111")]
+    [InlineData(0x4A, "00000000")]
+    [InlineData(0xF0, "10110010")]
+    [InlineData(0xFF, "10010111")]
+    public void ShouldNotAffectFlags(ushort input, string flags) {
+      byte f = Convert.ToByte(flags, 2);
+
+      AllOpcodes(
+        operands => {
+          operands[0].Target = input;
+          cpu.registers.f = f;
+        },
+        operands => Assert.Equal(f, cpu.registers.f));
     }
     
   }
